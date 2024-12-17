60a4be8744a6907e3f8226c4ab40b683433fbea3
BlackCosmos7 <xenia.ksu2018@yandex.ru>
2024-12-18 00:51:24 +0300

    Sword

--- COmpany 1/Assets/Scripts/SwordAttack.cs
+++ COmpany 1/Assets/Scripts/SwordAttack.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class SwordAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
 
-    // Update is called once per frame
-    void Update()
+    public Transform attackPos;
+    public LayerMask enemy;
+    public float attackRange;
+    public int damage;
+    public Animator anim;
+
+    private void Update()
     {
+        if(timeBtwAttack <= 0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                anim.SetTrigger("SwordAttack"); //возможно ошибка
+                Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemy);
+                for (int i = 0; i < enemies.Length; i++)
+                {
+                    enemies[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+            }
+            timeBtwAttack = startTimeBtwAttack;
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
         
     }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+    
 }
